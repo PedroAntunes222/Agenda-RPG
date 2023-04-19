6f1977e9690087d84555a632bcad522e2aac0e05
unknown <antunespedro222@gmail.com>
2023-04-18 23:53:36 -0300

    feat: biblioteca moment adicionada

--- pages/Tarefas/components/DataTarefa.jsx
+++ pages/Tarefas/components/DataTarefa.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Text,
   View,
@@ -8,99 +8,75 @@ import {
   ScrollView,
   Dimensions,
 } from "react-native";
+import moment from "moment";
 
 export default function DataTarefa({ data, hora }) {
-  
-    let colorData;
+  let colorData;
 
-    const dataAtual = new Date();
-    dataAtual.setHours(0, 0, 0, 0);
+  const dataRecebida = moment(data, "DD/MM/YYYY");
+  const dataRecebidaFormat = dataRecebida.format("DD/MM/YYYY");
+  // console.log(dataRecebidaFormat);
 
-    const dataRecebida = data;
-    const dataRepartida = dataRecebida.split("/");
-    const dataFinal = new Date(
-      `${dataRepartida[2]}-${dataRepartida[1]}-${dataRepartida[0]}`
-    );
+  const dataAtual = moment();
+  const dataAtualFormat = dataAtual.format("DD/MM/YYYY");
+  // console.log(dataAtualFormat);
 
-    // const horaAtual = new Date();
- 
-    // const horaRecebida = hora;
-    // const horaRepartida = horaRecebida.split(":");
-    // const horaFinal = new Date(horaAtual.getFullYear(), horaAtual.getMonth(), horaAtual.getDate(), horaRepartida[0], horaRepartida[1], 0, 0);
+  const horaAtual = moment();
+  const horaAtualFormat = horaAtual.format("HH:mm");
+  // console.log(horaAtualFormat);
 
-    if (dataAtual < dataFinal) {
-      // console.log("o dia ainda n chegou");
-      colorData = 'blue';
+  const horaRecebida = moment(hora, "HH:mm");
+  const horaRecebidaFormat = horaRecebida.format("HH:mm");
+  // console.log(horaRecebidaFormat);
+
+  if (dataAtualFormat < dataRecebidaFormat) {
+    // console.log("o dia ainda n chegou");
+    colorData = "blue";
+  }
+  if (dataAtualFormat > dataRecebidaFormat) {
+    // console.log("o dia já passou");
+    colorData = "#990e1f";
+  }
+
+  if (dataAtualFormat === dataRecebidaFormat) {
+    // console.log("o dia é hoje");
+    colorData = "green";
+
+    if (horaAtualFormat > horaRecebidaFormat) {
+      // console.log("a hora ja passou");
+      colorData = "#990e1f";
     }
-    if (dataAtual >= dataFinal) {
-      // console.log("o dia é hoje");
-      colorData = 'green';
-      // if (horaAtual > horaFinal) {
-      //   // console.log("a hora ja passou");
-      //   colorData = '#990e1f';
-      // }
-    }
-    if (dataAtual > dataFinal) {
-      // console.log("o dia já passou");
-      colorData = 'green';
-    }
+  }
 
-    const meses = [
-      "Janeiro",
-      "Fevereiro",
-      "Março",
-      "Abril",
-      "Maio",
-      "Junho",
-      "Julho",
-      "Agosto",
-      "Setembro",
-      "Outubro",
-      "Novembro",
-      "Dezembro",
-    ];
-    let dataSeparada = dataRecebida.split("/");
-    let dia = dataSeparada[0];
-    let mes = dataSeparada[1];
-    let mesExtenso = meses[mes - 1].slice(0, 3);
+  const meses = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
 
-    return (
-      <View style={[style.cardData, {backgroundColor: colorData}]}>
-        <Text style={{ color: "white", fontSize: 24 }}>{dia}</Text>
-        <Text style={{ color: "white", fontSize: 20 }}>{mesExtenso}</Text>
-      </View>
-    );
+  const dia = dataRecebida.format('DD');
+  const mes = dataRecebida.format('MM');
+  const mesExtenso = meses[mes-1].slice(0, 3);
+  console.log(dia)
+
+  return (
+    <View style={[style.cardData, { backgroundColor: colorData }]}>
+      <Text style={{ color: "white", fontSize: 24 }}>{dia}</Text>
+      <Text style={{ color: "white", fontSize: 20 }}>{mesExtenso}</Text>
+    </View>
+  );
 }
 
 const style = StyleSheet.create({
-  lista: {
-    flexDirection: "column",
-    gap: 10,
-    padding: 15,
-  },
-  card: {
-    height: 100,
-    padding: 10,
-    borderRadius: 10,
-    flexDirection: "row",
-    backgroundColor: "#323232",
-    justifyContent: "space-between",
-  },
-  cardInfos: {
-    flexDirection: "column",
-    justifyContent: "space-between",
-  },
-  cardInfo: {
-    flexDirection: "row",
-    gap: 10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  cardAtributo: {
-    width: 20,
-    height: 20,
-    borderRadius: 100,
-  },
   cardData: {
     // backgroundColor: "#1e1e1e",
     borderRadius: 10,
