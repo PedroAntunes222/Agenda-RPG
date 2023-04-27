5aa1f8582f7f55dffe9bae5c08ff11a7387f0017
unknown <antunespedro222@gmail.com>
2023-04-27 13:51:35 -0300

    fix: preparando tela de equipamento

--- pages/Personagem/Equipamentos/Equipamento.jsx
+++ pages/Personagem/Equipamentos/Equipamento.jsx
@@ -14,53 +14,54 @@ import { getItens, addItem } from "../../../Database/ItemDatabase";
 import Item from "../../../class/item";
 import { LoadingContext } from "../../../context/loading";
 
-export default function Equipamento({navigation}) {
+export default function Equipamento({ navigation }) {
   const [db, setDb] = useState(SQLite.openDatabase("agenda.db"));
   const [itens, setItens] = useState();
   const [equipamento, setEquipamento] = useState();
-  const {loading, setLoading} = useContext(LoadingContext);
+  const { loading, setLoading } = useContext(LoadingContext);
 
   useEffect(() => {
     getItens(setItens);
     getEquipamento(setEquipamento);
     setLoading(false);
   }, [db]);
-  
+
   useEffect(() => {
     // atualiza lista ao voltar
     navigation.addListener("focus", () => {
-        getItens(setItens);
-        setLoading(false);
+      getItens(setItens);
+      setLoading(false);
     });
   }, [navigation]);
 
-
   const adicionaItem = () => {
-    let novoItem = new Item(
-        1,
-        'espada',
-        'uma espada',
-        'raro',
-        'arma',
-        ''
-    );
-    addItem(novoItem)
+    let novoItem = new Item(1, "espada", "uma espada", "raro", "arma", "");
+    addItem(novoItem);
   };
 
   return (
-    itens!==undefined &&
-    <View>
-      {itens.map((item, index) => (
-        <View key={index}>
-            <Text>{item.nome}</Text>
-            <Text>{item.descricao}</Text>
-            <Text>{item.raridade}</Text>
-            <Text>{item.tipo}</Text>
-        </View>
-      ))}
+    itens !== undefined && (
+      <View>
+        {itens.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => {
+              //funcao para adicionar equipamento
 
-      <Button onPress={() => adicionaItem()} title="addItem"/>
-      
-    </View>
+              // setLoading(true);
+            }}
+          >
+            <View>
+              <Text>{item.nome}</Text>
+              <Text>{item.descricao}</Text>
+              <Text>{item.raridade}</Text>
+              <Text>{item.tipo}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+
+        <Button onPress={() => adicionaItem()} title="addItem" />
+      </View>
+    )
   );
 }
--- pages/Personagem/Equipamentos/EquipamentoRotas.jsx
+++ pages/Personagem/Equipamentos/EquipamentoRotas.jsx
@@ -1,28 +1,28 @@
-
 import React from "react";
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Equipamento from "./Equipamento";
 
 export default function EquipamentoRotas() {
-    
   const Stack = createNativeStackNavigator();
 
   return (
-            <Stack.Navigator
-              screenOptions={{
-                contentStyle:{backgroundColor:'#1E1E1E'},
-                headerStyle: {backgroundColor: '#1E1E1E'},
-                headerTintColor: '#fff',
-                headerTitleStyle:{fontSize:20}
-              }}
-            >
-
-              <Stack.Screen name="ListaItens" component={Equipamento} options={{ 
-                  title: 'Inventario',
-                  headerShown: false
-              }} />
-
-            </Stack.Navigator>
-  )
+    <Stack.Navigator
+      screenOptions={{
+        contentStyle: { backgroundColor: "#1E1E1E" },
+        headerStyle: { backgroundColor: "#1E1E1E" },
+        headerTintColor: "#fff",
+        headerTitleStyle: { fontSize: 20 },
+      }}
+    >
+      <Stack.Screen
+        name="ListaItens"
+        component={Equipamento}
+        options={{
+          title: "Inventario",
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
 }
